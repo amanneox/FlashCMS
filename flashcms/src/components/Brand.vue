58c9ede85c121963b67fe4b333ee8be765211ae8
Aman Adhikari <amanadhikari2@gmail.com>
2018-09-12 19:18:17 +0530

    Added Logo

--- flashcms/src/components/Brand.vue
+++ flashcms/src/components/Brand.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-  <el-row justify="start">
-  <el-col justify="start" :span="6"> <span>ADD LOGO HERE</span> </el-col>
+  <el-row class="brand-row" justify="start">
+  <el-col justify="start" :span="6"> <img width="100" height="100" src="../assets/ic_logo.png" alt=""> </el-col>
 </el-row>
 </template>
 
@@ -11,4 +11,7 @@ export default {
 </script>
 
 <style lang="css">
+.brand-row{
+  margin: 0 !important;
+}
 </style>
